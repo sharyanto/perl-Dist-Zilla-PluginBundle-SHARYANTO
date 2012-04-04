207d005ab6c6dc4c70b163ca9f76f4604a1f2a0b
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-04-04 17:40:05 +0700

    Add bundle: DZP::SHARYANTO::Task

---
+++ lib/Dist/Zilla/PluginBundle/SHARYANTO/Task.pm
@@ -0,0 +1,46 @@
+package Dist::Zilla::PluginBundle::SHARYANTO::Task;
+
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# VERSION
+
+use Dist::Zilla::PluginBundle::Filter;
+
+sub configure {
+    my $self = shift;
+
+    $self->add_bundle(Filter => {
+        -bundle => '@SHARYANTO',
+        -remove => [qw/PodCoverageTests PodSyntaxTests PodWeaver/],
+    });
+
+    $self->add_plugins(
+        'TaskWeaver',
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+# ABSTRACT: Dist::Zilla like SHARYANTO when you build your task dists
+
+=head1 SYNOPSIS
+
+ # dist.ini
+ [@SHARYANTO::Task]
+
+is equivalent to:
+
+ [@Filter]
+ bundle=@SHARYANTO
+ remove=PodCoverageTests
+ remove=PodSyntaxTests
+ remove=PodWeaver
+
+ [TaskWeaver]
+
+
+=head1 DESCRIPTION
+
+=cut
