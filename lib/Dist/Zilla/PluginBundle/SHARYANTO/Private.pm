fa0c1b15e0d89d23553275009663157a1cf8e288
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-11 20:46:19 +0700

    Add bundle: SHARYANTO::Private, SHARYANTO::Task::Private

---
+++ lib/Dist/Zilla/PluginBundle/SHARYANTO/Private.pm
@@ -0,0 +1,39 @@
+package Dist::Zilla::PluginBundle::SHARYANTO::Private;
+
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# VERSION
+
+use Dist::Zilla::PluginBundle::Filter;
+
+sub configure {
+    my $self = shift;
+
+    $self->add_bundle(Filter => {
+        -bundle => '@SHARYANTO',
+        -remove => [qw/ConfirmRelease UploadToCPAN/],
+    });
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+# ABSTRACT: Dist::Zilla like SHARYANTO when you build your private dists
+
+=head1 SYNOPSIS
+
+ # dist.ini
+ [@SHARYANTO::Private]
+
+is equivalent to:
+
+ [@Filter]
+ bundle=@SHARYANTO
+ remove=ConfirmRelease
+ remove=UploadToCPAN
+
+
+=head1 DESCRIPTION
+
+=cut
---
+++ lib/Dist/Zilla/PluginBundle/SHARYANTO/Task/Private.pm
@@ -0,0 +1,39 @@
+package Dist::Zilla::PluginBundle::SHARYANTO::Task::Private;
+
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# VERSION
+
+use Dist::Zilla::PluginBundle::Filter;
+
+sub configure {
+    my $self = shift;
+
+    $self->add_bundle(Filter => {
+        -bundle => '@SHARYANTO::Task',
+        -remove => [qw/ConfirmRelease UploadToCPAN/],
+    });
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+# ABSTRACT: Dist::Zilla like SHARYANTO when you build your private task dists
+
+=head1 SYNOPSIS
+
+ # dist.ini
+ [@SHARYANTO::Task::Private]
+
+is equivalent to:
+
+ [@Filter]
+ bundle=@SHARYANTO::Task
+ remove=ConfirmRelease
+ remove=UploadToCPAN
+
+
+=head1 DESCRIPTION
+
+=cut
